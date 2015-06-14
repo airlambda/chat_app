dd89a581b27b0ebfd482fc49208f4e65e991622a
airlambda <airlambda@users.noreply.github.com>
2015-06-14 19:50:28 +0100

    Update index.js
    Again, added __dirname to line 6 of index.js. Still does not work?

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
 
-io.on('connection', function(socket){
+/*io.on('connection', function(socket){
 		console.log('A user connected');
 		socket.on('disconnect', function(){
     		console.log('user disconnected');
   });
-});
-
+});*/
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
